200e7faa6169a336f7d3aeee7737aaed319d7359
Tony Xin <xyytonyxin@gmail.com>
2022-07-18 16:42:30 -0500

    notifications friend requests

---
+++ frontend/components/FriendRequest.vue
@@ -0,0 +1,45 @@
+
+<template>
+    <v-banner>
+        <div class="tw-flex tw-flex-row">
+            <ProfileImage :src="friendRequest.profileImage" :width="30" class="tw-mr-2"></ProfileImage>
+            <div>
+            <h1>{{ friendRequest.username }}</h1>
+            <h1 class="tw-text-xs tw-text-gray-400">{{ friendRequest.principalId }}</h1>
+            </div>
+            <v-spacer></v-spacer>
+            <v-btn color="success" dark fab small class="tw-mr-2"><v-icon>mdi-check</v-icon></v-btn>
+            <v-btn color="red" dark fab small><v-icon>mdi-close</v-icon></v-btn>
+        </div>
+    </v-banner>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+import ProfileImage from './ProfileImage.vue'
+
+export default {
+    name: 'FriendRequest',
+
+    components: {
+        ProfileImage
+    },
+
+    props: {
+        friendRequest: { type: Object, required: true },
+    },
+
+    data: () => ({
+    }),
+
+    computed: {},
+
+    methods: {
+        
+    },
+
+    created() { },
+}
+</script>
---
+++ frontend/components/Notifications.vue
@@ -0,0 +1,57 @@
+
+<template>
+    <v-dialog v-model="dialog" width="500">
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn icon color="white" v-if="true" v-bind="attrs" v-on="on">
+                <v-badge color="red" :content="6" bottom overlap>
+                    <v-icon>mdi-bell</v-icon>
+                </v-badge>
+            </v-btn>
+        </template>
+
+        <v-card>
+
+            <v-card-text class="tw-pt-3">
+                <FriendRequest v-for="(friendRequest, i) in friendRequests" :friendRequest="friendRequest" :key="i"></FriendRequest>
+            </v-card-text>
+
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="red" text @click="dialog = false">
+                    Cancel
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+import FriendRequest from './FriendRequest.vue'
+
+export default {
+    name: 'Notifications',
+
+    components: {
+        FriendRequest
+    },
+
+    props: {
+        friendRequests: { type: Array, required: true },
+    },
+
+    data: () => ({
+        dialog: false,
+    }),
+
+    computed: {},
+
+    methods: {
+        
+    },
+
+    created() { },
+}
+</script>
