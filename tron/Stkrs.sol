a99a4b69032d659699e87c8d27867485e8fcaa5e
Arjun Patrawala <arjunpatrawala@berkeley.edu>
2022-07-21 11:36:34 -0700

    stkrs should be fixed

--- tron/Stkrs.sol
+++ tron/Stkrs.sol
@@ -2,78 +2,56 @@
 pragma solidity ^0.8.6;
 
 contract Stkrs {
-  struct Stkr {
-    uint id;
-    address creator;
-    string title;
-    string organization;
-    string description;
-    string category;
-    string image;
-  }
+  address[] public stkrCreator;
+  mapping(address => uint[]) public pins;
 
-  /* struct Comment {
-    address creator;
-    string content;
-    uint createdAt;
-  } */
+  event UserUpdated(address addr, string name, string bio, string profile_image, string telegram_username);
+  event StkrCreated(uint id, string title, string organization, string description, string category, string image);
+  event FriendRequest(address from, address to);
+  event StkrSent(address to, uint stkr);
+  event Comment(address to, address creator, string content);
 
-  struct UserData {
-    string name;
-    string bio;
-    string profile_image;
-  }
-
-  mapping(address => UserData) public users;
-  Stkr[] public stkrs;
-
-  function setUser(string memory name, string memory bio, string memory profile_image) public {
-    users[msg.sender] = UserData(name, bio, profile_image);
+  function updateUser(string memory name, string memory bio, string memory profile_image, string memory telegram_username) public {
+    emit UserUpdated(msg.sender, name, bio, profile_image, telegram_username);
   }
 
   function createStkr(string memory title, string memory organization, string memory description, string memory category, string memory image) public returns (uint) {
-    Stkr memory s = Stkr(stkrs.length, msg.sender, title, organization, description, category, image);
-    stkrs.push(s);
-    return s.id;
+    uint id = stkrCreator.length;
+    emit StkrCreated(id, title, organization, description, category, image);
+    stkrCreator.push(msg.sender);
+    return id;
   }
-
-  event FriendRequest(address indexed from, address indexed to);
 
   function sendFriendRequest(address to) public {
     require(msg.sender != to, "Cannot friend yourself");
     emit FriendRequest(msg.sender, to);
   }
 
-  event StkrSent(address indexed to, uint indexed stkr);
-
   function sendStkr(uint stkr, address to) public {
-    require(stkrs.length > stkr, "Stkr does not exist");
-    require(stkrs[stkr].creator == msg.sender, "Not creator of stkr");
+    require(stkrCreator.length > stkr, "Stkr does not exist");
+    require(stkrCreator[stkr] == msg.sender, "Not creator of stkr");
 
     emit StkrSent(to, stkr);
   }
-
-  event Comment(address indexed to, address creator, string content);
 
   function addComment(address user, string memory content) public {
     emit Comment(user, msg.sender, content);
   }
 
-  mapping(address => uint[]) public pins;
   function addPin(uint stkr) public {
     require(pins[msg.sender].length < 5, "Too many pins");
-    require(stkrs.length > stkr, "Invalid stkr");
+    require(stkrCreator.length > stkr, "Invalid stkr");
     pins[msg.sender].push(stkr);
   }
 
   function removePin(uint stkr) public {
     require(pins[msg.sender].length > 0, "No pins");
-    require(stkrs.length > stkr, "Invalid stkr");
+    require(stkrCreator.length > stkr, "Invalid stkr");
 
     for (uint i = 0; i < pins[msg.sender].length; i++) {
       if (pins[msg.sender][i] == stkr) {
         pins[msg.sender][i] = pins[msg.sender][pins[msg.sender].length - 1];
-        delete pins[msg.sender][pins[msg.sender].length - 1];
+        pins[msg.sender].pop();
       }
     }
   }
